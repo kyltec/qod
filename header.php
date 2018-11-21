50b2ed557b74a23b138e07ba59411ee12462a9a8
Kyle_Tecson <kyltecs@gmail.com>
2018-11-20 16:47:59 -0800

    update header and body

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
+	
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
@@ -26,10 +27,9 @@
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+				<div class="header-img">
+					<a href="<?php echo home_url();?>"> <img src="<?php echo get_template_directory_uri()?>/images/qod-logo.svg" alt=""></a>
+				</div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
